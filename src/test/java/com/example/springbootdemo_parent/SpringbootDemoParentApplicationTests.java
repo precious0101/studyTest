9b843d5233682f11b2ffc739a65e4a6e0806e8e5
precious0101 <1007146244@qq.com>
2023-02-06 15:56:58 +0800

    初始化项目

---
+++ src/test/java/com/example/springbootdemo_parent/SpringbootDemoParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo_parent;
+
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoParentApplicationTests {
+
+  /*  @Test
+    void contextLoads() {
+    }*/
+
+}
